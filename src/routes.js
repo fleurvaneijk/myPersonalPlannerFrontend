4081f6b0d4eb405c62d46c9424bd75a4318e6596
Simon <nomisvank@hotmail.com>
2020-05-13 16:29:21 +0200

    router fixes

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,26 @@ import AgendaComponent from './components/agenda/Agenda.vue';
 import MenuComponent from './components/menu/Menu.vue';
 import PlannerComponent from './components/planner/Planner.vue';
 import SettingsComponent from './components/settings/Settings.vue';
+import HeaderComponent from './components/Header'
 
 export const routes = [
-  { path: '/', component: MenuComponent },
-  { path: '/agenda', component: AgendaComponent },
+  { path: '' , name: 'menu', components: {
+    default: MenuComponent,
+    'header-top': HeaderComponent
+  } },
+  { path: '/agenda' , name: 'agenda', components: {
+      default: AgendaComponent,
+      'header-top': HeaderComponent
+    } },
+  { path: '/planner' , name: 'planner', components: {
+      default: PlannerComponent,
+      'header-top': HeaderComponent
+    } },
+  { path: '/settings' , name: 'settings', components: {
+      default: SettingsComponent,
+      'header-top': HeaderComponent
+    } },
   { path: '/login', component: LoginComponent },
   { path: '/signup', component: SignUpComponent },
-  { path: '/planner', component: PlannerComponent },
-  { path: '/settings', component: SettingsComponent },
+  { path: '*', redirect: '/' }
 ];
