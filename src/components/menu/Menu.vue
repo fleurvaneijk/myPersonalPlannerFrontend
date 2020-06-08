a05f7d40cc2383268294eb87bbaed6914d9520cf
Simon <nomisvank@hotmail.com>
2020-06-08 09:38:59 +0000

    Feature/totd

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,11 +1,82 @@
 <template>
-  <div class="nav-component">
-    <h1>Menu</h1>
+  <div class="background">
+    <div class="wrapper nav-component">
+      <div class="container">
+        <div class="row">
+          <h1>Menu</h1>
+        </div>
+        <br>
+        <div class="row">
+          <div class="col-sm-5 col-md-5 column">
+            <task-of-the-day></task-of-the-day>
+          </div>
+          <div class=".col-sm2 col-md-2"></div>
+          <div class="col-sm-5 col-md-5 column">
+            <weather></weather>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
+  import Totd from "./TaskOfTheDay";
+  import Weather from "./Weather";
 
+  export default {
+    name: 'Menu',
+    components: {
+      "task-of-the-day" : Totd,
+      "weather" : Weather,
+    },
   }
 </script>
+<style scoped lang="scss">
+  @import "src/variables";
+
+  .background {
+    background-image: url("../../assets/background2.jpg");
+    background-size: cover;
+  }
+
+  .wrapper {
+    background-color: rgba(255, 255, 255, 0.75);
+    margin: 0 15%;
+    /*min-width: 1050px;*/
+
+    h1 {
+      padding-top: 30px;
+    }
+
+    p {
+      font-weight: bold;
+      font-size: 1.5em;
+    }
+  }
+
+  .container {
+    text-align: center;
+  }
+
+  .column {
+    border: 2px #fcd8ea dashed;
+    height: 70%;
+    min-height: 500px;
+    background-color: rgba(255, 255, 255, 0.7);
+
+  }
+
+  @media(max-width:1620px){
+    .wrapper {
+      margin: 0 5%;
+    }
+  }
+
+  @media(max-width:1260px){
+    .wrapper {
+      margin: 0 1%;
+    }
+  }
+
+</style>
---
+++ src/components/menu/Weather.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Weather</h1>
+    <IFRAME SRC="//gadgets.buienradar.nl/gadget/forecastandstation/6260" NORESIZE SCROLLING=NO HSPACE=0 VSPACE=0 FRAMEBORDER=0 MARGINHEIGHT=0 MARGINWIDTH=0 WIDTH=300 HEIGHT=190></IFRAME>
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+
+  }
+</script>
+<style scoped lang="scss">
+  IFRAME {
+    margin: 50px;
+  }
+</style>
